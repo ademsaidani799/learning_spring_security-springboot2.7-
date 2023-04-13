2cd3fd559c03136732d7db1a122ff5b6de1bfc1a
adem saidani <ademsaidani799@gmail.com>
2023-04-13 03:20:14 +0200

    securing an api with basic auth

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.security;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,12 +10,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableAutoConfiguration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-
-
-
-
-
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
 }
