4aed948f676e87f45f695d6ba57646b1698609a0
adem saidani <ademsaidani799@gmail.com>
2023-05-08 14:08:57 +0200

    creating the ApplicationUserDao and using it to retrun the user by username

--- src/main/java/com/example/demo/auth/ApplicationUserService.java
+++ src/main/java/com/example/demo/auth/ApplicationUserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +8,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationUserService implements UserDetailsService {
+
+    private final ApplicationUserDao applicationUserDao;
+
+
+    public ApplicationUserService(ApplicationUserDao applicationUserDao) {
+        this.applicationUserDao = applicationUserDao;
+    }
+
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return applicationUserDao.selectApplicationUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(""));
     }
 }
